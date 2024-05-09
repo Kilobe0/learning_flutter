a8617e7ed9f4c27b3fc6813247340dee45142517
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2024-05-09 11:26:05 -0300

    Service

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/app/interfaces/local_storage_interface.dart';
+import 'package:learning_flutter/app/services/shared_local_storage_service.dart';
 
 import 'models/appconfig_model.dart';
 
@@ -7,13 +9,24 @@ class AppController {
   static final AppController instance = AppController._();
 
   // Construtor privado para garantir que a classe seja um singleton.
-  AppController._();
+  AppController._() {
+    storage.get('isDark').then(
+      (value) {
+        if (value != null) {
+          config.themeSwitch.value = value;
+        }
+      },
+    );
+  }
 
   final AppConfigModel config = AppConfigModel();
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
+  final ILocalStorage storage = SharedLocalStorageService();
+
   void changeTheme(bool value) {
     config.themeSwitch.value = value;
+    storage.put('isDark', value);
   }
 }
