1649c07d46be38038f645a38d81731c996224a00
Kilobe0 <104333874+Kilobe0@users.noreply.github.com>
2024-05-09 14:14:31 -0300

    Refactor getWeather method in ApiadvisorRepository

--- lib/app/repositories/apiadvisor_repository.dart
+++ lib/app/repositories/apiadvisor_repository.dart
@@ -10,14 +10,10 @@ class ApiadvisorRepository implements IApiAdvisor {
 
   @override
   Future<ApiadvisorModel> getWeather() async {
-    try {
-      var json = await client.get(
-          'http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=e4e9d0d4141a4549fa6f1ba827e51f6e');
-      ApiadvisorModel model = ApiadvisorModel.fromJson(json[0]);
-      return model;
-    } catch (e) {
-      print(e);
-    }
-    return ApiadvisorModel();
+    var json = await client.get(
+        'http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=e4e9d0d4141a4549fa6f1ba827e51f6e');
+    ApiadvisorModel model = ApiadvisorModel.fromJson(json[0]);
+    
+    return model;
   }
 }
